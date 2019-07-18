e2cc6e18e655e561ffc4406e95660a44ceece04a
push0012 <pushpamal0012@gmail.com>
2019-07-18 11:13:59 +0530

    Add Redirection to root

--- app/Http/Controllers/ContactUsController.php
+++ app/Http/Controllers/ContactUsController.php
@@ -10,9 +10,9 @@ class ContactUsController extends Controller
 {
     public function sendMail(Request $request){
         //echo $request;
-        //return redirect('/');
+        
         Mail::send(new ContactUsMail($request));
-        //
+        return redirect('/');
 
     }
 }
