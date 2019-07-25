46696363364d29a98c6478be84661142e80aa308
push0012 <pushpamal0012@gmail.com>
2019-07-25 14:26:39 +0530

    Add new Information mailable class to work with RTI email

---
+++ app/Mail/Information.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Http\Request;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+class Information extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $email;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct(Request $request)
+    {
+        $this->email = $request;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->subject('Right to Information Inquiry')
+                    ->from('hdcspspc@gmail.com', 'Administrator RTI')
+                    ->to('pushpamal0012@gmail.com')
+                    ->view('interact.inquiry');
+    }
+}
