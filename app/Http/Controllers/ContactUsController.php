a02c41cbb9e25b52e409e92a2301812986f9a457
push0012 <pushpamal0012@gmail.com>
2019-07-25 14:25:05 +0530

    add new sendInquiry function to work with Information Mail class and rti form

--- app/Http/Controllers/ContactUsController.php
+++ app/Http/Controllers/ContactUsController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Mail\ContactUsMail;
+use App\Mail\Information;
 use Illuminate\Support\Facades\Mail;
 
 class ContactUsController extends Controller
@@ -12,6 +13,14 @@ class ContactUsController extends Controller
         //echo $request;
         
         Mail::send(new ContactUsMail($request));
+        return redirect('/');
+
+    }
+    
+    public function sendInquiry(Request $request){
+        //echo $request;
+        
+        Mail::send(new Information($request));
         return redirect('/');
 
     }
