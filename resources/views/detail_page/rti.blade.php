95596113abc72fd3b00bed94c47c86184bd5f68e
push0012 <pushpamal0012@gmail.com>
2019-08-01 11:31:52 +0530

    add new page section to attach pdfs

--- resources/views/detail_page/rti.blade.php
+++ resources/views/detail_page/rti.blade.php
@@ -41,48 +41,48 @@
         <h5 style="font-size: 1.1em;">තොරතුරු පනත හරහා තොරතුරු ලබාගැනීමේ ලේඛන.</h5>
         <div class="container">
             <div class="row bg-dark" style="height: 30px; color: white; padding-top: 4px; margin-bottom: 20px;">
-                <div class="col-md-2" style="">
+                <div class="col-md-1" style="">
                     #
                 </div>
-                <div class="col-md-10">
+                <div class="col-md-11">
                     ලේඛන නාම
                 </div>
             </div>
             <div class="row" style="height: 40px;">
-                <div class="col-md-2">
+                <div class="col-md-1">
                     01
                 </div>
-                <div class="col-md-10">
+                <div class="col-md-11">
                     <a href="{{ asset('file/rti_act.pdf') }}" target="_blank">
                         තොරතුරු දැන ගැනීමේ අයිතිවාසිකම පිළිබඳ පනත
                     </a>
                 </div>
             </div>
             <div class="row" style="height: 40px;">
-                <div class="col-md-2">
+                <div class="col-md-1">
                     02
                 </div>
-                <div class="col-md-10">
+                <div class="col-md-11">
                     <a href="{{ asset('file/rti_act_order.pdf') }}" target="_blank">
                         තොරතුරු දැන ගැනීමේ අයිතිවාසිකම පිළිබඳ පනතේ නියෝග
                     </a>
                 </div>
             </div>
             <div class="row" style="height: 40px;">
-                <div class="col-md-2">
+                <div class="col-md-1">
                     03
                 </div>
-                <div class="col-md-10">
+                <div class="col-md-11">
                     <a href="{{ asset('file/RTI_01.pdf') }}" target="_blank">
                         තොරතුරු ලබාගැනීමේ අයදුම් පත්‍රය (RTI 01)
                     </a>
                 </div>
             </div>
             <div class="row" style="height: 40px;">
-                <div class="col-md-2">
+                <div class="col-md-1">
                     04
                 </div>
-                <div class="col-md-10">
+                <div class="col-md-11">
                     <a href="{{ asset('file/RTI_10.pdf') }}" target="_blank">
                         නම් කල නිලධාරියා වෙත යවන අභියාචනා අයදුම් පත්‍රය (RTI 10)
                     </a>
@@ -95,6 +95,20 @@
                 <li><a href="https://www.rti.gov.lk" target="_blank">තොරතුරු පනත නිල වෙබ් අඩවිය</a></li>
                 <li><a href="http://www.rtiwatch.lk" target="_blank">Transparency International Sri Lanka</a></li>
             </ul>
+            <hr>
+            <h5 style="font-size: 1.1em;">කාර්යාලය මගින් ජනතාවට ප්‍රදානය කරනු ලබන පොදු තොරතුරු</h5>
+            <div class="container">
+                <div class="row" style="height: 40px;">
+                    <div class="col-md-1">
+                        01
+                    </div>
+                    <div class="col-md-11">
+                        <a href="{{ asset('file/rti_act.pdf') }}" target="_blank">
+                            තොරතුරු දැන ගැනීමේ අයිතිවාසිකම පිළිබඳ පනත
+                        </a>
+                    </div>
+                </div>
+            </div>
         </div>  
     </div>
 </div>
