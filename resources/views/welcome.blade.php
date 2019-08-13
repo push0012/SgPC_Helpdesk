6becea12697926d10151ccfee1a59d8c20b1778f
push0012 <pushpamal0012@gmail.com>
2019-08-13 09:14:22 +0530

    add style link type to text/css

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,14 +8,14 @@
 
     <link rel="icon" type="image/png" href="{{ asset('image/sg_info.png') }}" />
     
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" type="text/css">
-    <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
+    <link type="text/css" href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" type="text/css">
+    <link type="text/css" href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
 
-    <link href="{{ asset('css/style1.css') }}" rel="stylesheet"> 
-    <link href="{{ asset('css/home.css') }}" rel="stylesheet"> 
-    <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet"> 
-    <link rel="stylesheet" href="css/fontawesome/css/all.css">
-    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link type="text/css" href="{{ asset('css/style1.css') }}" rel="stylesheet"> 
+    <link type="text/css" href="{{ asset('css/home.css') }}" rel="stylesheet"> 
+    <link type="text/css" href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet"> 
+    <link type="text/css" rel="stylesheet" href="css/fontawesome/css/all.css">
+    <link type="text/css" rel="dns-prefetch" href="//fonts.gstatic.com">
 
     <script src="{{asset('js/bootstrap.min.js')}}"></script>
     <script src="{{ asset('js/jquery-3.3.1.min.js') }}"></script>
