fa722b307303c015d9fcf75d78ba67cbbdcf04e7
push0012 <pushpamal0012@gmail.com>
2019-12-04 15:56:38 +0530

    add store function and import some model classes

--- app/Http/Controllers/GraduationController.php
+++ app/Http/Controllers/GraduationController.php
@@ -5,6 +5,10 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\DsDivision;
 use App\College;
+use App\Student;
+use App\StudentEducationDegree;
+use App\StudentProfessional;
+use App\StudentLanguage;
 
 class GraduationController extends Controller
 {
@@ -12,5 +16,26 @@ class GraduationController extends Controller
     {
         $universities = College::where('clg_type','university')->get();
         return view('insert_form.graduate')->with('colleges',$universities);
+    }
+    public function store(Request $request)
+    {
+        $student = Student::create($request->all());
+        $request->request->add(['stu_id' => $student->stu_id]);
+        $students = StudentEducationDegree::create($request->all());
+        $studentss = StudentProfessional::create($request->all());
+
+        $languages = $request->language;
+
+        foreach ($languages as $language){ 
+            StudentLanguage::create([
+                'language_name' => $language['language_name'],
+                'write_skill' => $language['write_skill'],
+                'read_skill' => $language['read_skill'],
+                'speech_skill' => $language['speech_skill'],
+                'stu_id' => $student->stu_id
+                ]);
+        }
+
+        return response()->json($studentss, 201);
     }
 }
