f3dd8ce34e8962be272709e74484ac7a53020c47
push0012 <pushpamal0012@gmail.com>
2019-12-05 14:21:55 +0530

    add course type for where, add diploma show method

--- app/Http/Controllers/CollegeCourseController.php
+++ app/Http/Controllers/CollegeCourseController.php
@@ -49,10 +49,15 @@ class CollegeCourseController extends Controller
      */
     public function show($id)
     {
-        $cozbycollege = DB::table('collegecourseview')->where('clg_id', $id)->get();
+        $cozbycollege = DB::table('collegecourseview')->where('clg_id', $id)->where('cos_type','Degree')->get();
         return response()->json($cozbycollege, 201);
     }
 
+    public function show_diploma($id)
+    {
+        $cozbycollege = DB::table('collegecourseview')->where('clg_id', $id)->where('cos_type','Diploma')->get();
+        return response()->json($cozbycollege, 201);
+    }
     /**
      * Show the form for editing the specified resource.
      *
