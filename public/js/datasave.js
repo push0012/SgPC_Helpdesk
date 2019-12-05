b868900749ba76becb7b8ca42dc12e4f95d8f453
push0012 <pushpamal0012@gmail.com>
2019-12-05 14:59:33 +0530

    add diploma data store functions

--- public/js/datasave.js
+++ public/js/datasave.js
@@ -1,8 +1,7 @@
 $(document).ready(function() {
-
+    //Graduate Data Store
     $("#finish").click(function(){
-
-        var posts = [];
+        var posts = []; 
         $('#languageTable > tbody  > tr').each(function() {
             var postData = {
                 'language_name':$(this).find('#language_name').val(),
@@ -51,10 +50,62 @@ $(document).ready(function() {
             },
             error: function(msg) {
                 console.log(msg);
-               
             }
         });
         console.log(savedata);
+    });
 
+    //Diploma Data Store
+    $("#dfinish").click(function(){
+        var posts = []; 
+        $('#languageTable > tbody  > tr').each(function() {
+            var postData = {
+                'language_name':$(this).find('#language_name').val(),
+                'write_skill':$(this).find('#writting').val(),
+                'read_skill':$(this).find('#reading').val(),
+                'speech_skill' :$(this).find('#speech').val()
+            };
+            posts.push(postData);
+        });
+        var savedata = {
+            'title' : $('#title').val(),
+            'stu_name':$('#stu_name').val(),
+            'sex' : $('#sex').val(),
+            'dob':$('#dob').val(),
+            'nic' : $('#nic').val(),
+            'stu_address':$('#stu_address').val(),
+            'district' : $('#district').val(),
+            'dsa_id':$('#dsa_id').val(),
+            'stu_mobile' : $('#stu_mobile').val(),
+            'stu_telephone':$('#stu_telephone').val(),
+            'stu_email' : $('#stu_email').val(),
+
+            'clg_id':$('#d_clg_id').val(),
+            'cos_id' : $('#d_cos_id').val(),
+            'speciality_in_word':$('#speciality_in_word').val(),
+            'medium' : $('#medium').val(),
+            'effective_date':$('#effective_date').val(),
+            'other_details' : $('#other_details').val(),
+
+            'job_availability':$('#job_availability').val(),
+            'job_sector':$('#job_sector').val(),
+            'job_preference' : $('#job_preference').val(),
+            'language' : posts,
+            'it_skill':$('#it_skill').val(),
+            'it_skill_detail':$('#it_skill_detail').val(),
+            'extra_activity' : $('#extra_activity').val(),
+        }
+        $.ajax({
+            type: "POST",
+            url: '/diploma_register',
+            data: savedata,
+            success: function( msg ) {
+                console.log( msg);
+            },
+            error: function(msg) {
+                console.log(msg);
+            }
+        });
+        console.log(savedata);
     });
 });
