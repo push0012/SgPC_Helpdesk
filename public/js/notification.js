0ea315624c870633947166d5f3775ad4f866e0a7
push0012 <pushpamal0012@gmail.com>
2019-12-05 11:04:48 +0530

    first initialize for the nortifiaction function file

---
+++ public/js/notification.js
@@ -0,0 +1,30 @@
+function callNotification($icon, $title, $message,$type) {
+
+$.notify({
+    // options
+    icon: $icon,
+    title: $title,
+    message:  $message,
+},{
+    // settings
+    type: $type,
+    allow_dismiss: true,
+    placement: {
+        from: "bottom",
+        align: "right"
+    },
+    z_index: 1031,
+    delay: 2000,
+    timer: 1000,
+    animate: {
+        enter: 'animated fadeIn',
+        exit: 'animated fadeOut'
+    },
+    icon_type: 'class',
+    template: '<div data-notify="container" class="col-xs-11 col-sm-3 alert alert-{0}" role="alert">' +
+        '<button type="button" aria-hidden="true" class="close" data-notify="dismiss">×</button>' +
+        '<h6 data-notify="title"><span data-notify="icon"></span> {1}</h6> ' +
+        '<span data-notify="message">{2}</span>' +
+    '</div>' 
+});
+}
