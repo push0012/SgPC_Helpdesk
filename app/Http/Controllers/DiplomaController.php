94b4ff3eb69cb170d406ebf0ba596b94397098ea
push0012 <pushpamal0012@gmail.com>
2019-12-05 14:57:17 +0530

    add store function and import model classes

--- app/Http/Controllers/DiplomaController.php
+++ app/Http/Controllers/DiplomaController.php
@@ -4,6 +4,10 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\College;
+use App\Student;
+use App\StudentEducationDiploma;
+use App\StudentProfessional;
+use App\StudentLanguage;
 
 class DiplomaController extends Controller
 {
@@ -11,5 +15,27 @@ class DiplomaController extends Controller
     {
         $colleges = College::all();
         return view('insert_form.diploma')->with('colleges',$colleges);
+    }
+
+    public function store(Request $request)
+    {
+        $student = Student::create($request->all());
+        $request->request->add(['stu_id' => $student->stu_id]);
+        $students = StudentEducationDiploma::create($request->all());
+        $studentss = StudentProfessional::create($request->all());
+
+        $languages = $request->language;
+
+        foreach ($languages as $language){ 
+            StudentLanguage::create([
+                'language_name' => $language['language_name'],
+                'write_skill' => $language['write_skill'],
+                'read_skill' => $language['read_skill'],
+                'speech_skill' => $language['speech_skill'],
+                'stu_id' => $student->stu_id
+                ]);
+        }
+
+        return response()->json($studentss, 201);
     }
 }
