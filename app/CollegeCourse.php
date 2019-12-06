efb6db4505381c35a45425df6a644fcea23e0196
push0012 <pushpamal0012@gmail.com>
2019-12-06 15:56:33 +0530

    add cos_duration field

--- app/CollegeCourse.php
+++ app/CollegeCourse.php
@@ -11,7 +11,7 @@ class CollegeCourse extends Model
     //protected $primaryKey = 'clg_id, cos_id';
 
     protected $fillable = [
-        'cos_coor_name','cos_coor_mobile',
+        'cos_coor_name','cos_coor_mobile','cos_duration',
         'clg_id', 'cos_id','spc_id',
         'deleted','user_data'
     ];
