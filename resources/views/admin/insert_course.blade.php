32af0c519fa07d19b9577c722c871638979bc541
push0012 <pushpamal0012@gmail.com>
2019-12-10 11:55:48 +0530

    remove dynamic course input fill, change some input box parameters

--- resources/views/admin/insert_course.blade.php
+++ resources/views/admin/insert_course.blade.php
@@ -9,9 +9,9 @@
                 <div class="container-fluid">
                     <div class="form-group row">
                         <div class="col-md-12">
-                            <label for="cos_title">College</label><span id="cos_title-error" class="registration-error"></span>
+                            <label for="e_clg_id">College</label><span id="e_clg_id-error" class="registration-error"></span>
                             <div>
-                                <select class="form-control" name="clg_id" id="clg_id">
+                                <select class="form-control" name="e_clg_id" id="e_clg_id">
                                     <option value="None" disabled selected>Select University</option>
                                     @foreach($colleges as $college)
                                         <option value="{{ $college->clg_id}}">{{ $college->clg_name }}</option>
@@ -25,7 +25,10 @@
                             <label for="cos_id">College</label><span id="cos_id-error" class="registration-error"></span>
                             <div>
                                 <select class="form-control" name="cos_id" id="cos_id">
-                                    <option value="None" disabled selected>Select Degree</option>
+                                    <option value="None" disabled selected>Select Course</option>
+                                    @foreach($courses as $course)
+                                        <option value="{{ $course->cos_id}}">{{ $course->cos_title }}</option>
+                                    @endforeach
                                 </select>
                             </div>
                         </div>  
@@ -51,6 +54,13 @@
                                     </tr>
                                 </thead>
                             </table>
+                        </div>  
+                    </div>
+                    <div class="form-group row">
+                        <div class="col-md-12">
+                            <div>
+                                <input class="btnAction store" type="button" name="cos_store" id="cos_store" value="Store Courses" >
+                            </div>
                         </div>  
                     </div> 
                 </div>
