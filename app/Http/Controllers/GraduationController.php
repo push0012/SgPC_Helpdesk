781a9e5a1296faf00517e109c119353f8f3a8373
push0012 <pushpamal0012@gmail.com>
2019-12-13 09:44:26 +0530

    adding new cos and speciality codes are attached

--- app/Http/Controllers/GraduationController.php
+++ app/Http/Controllers/GraduationController.php
@@ -10,6 +10,9 @@ use App\StudentEducationDegree;
 use App\StudentProfessional;
 use App\StudentLanguage;
 use App\StudentConfirmDegree;
+use App\CollegeCourse;
+use App\Course;
+use App\DegreeSpecial;
 use DB;
 
 
@@ -24,6 +27,28 @@ class GraduationController extends Controller
     {
         $student = Student::create($request->all());
         $request->request->add(['stu_id' => $student->stu_id]);
+        
+        if(($request->cos_id == 0) && ($request->spc_id == 0))
+        {
+            $newCourse = Course::create(['cos_title'=> $request->new_cos,'cos_type' =>'Degree']);
+            $newSpecial = DegreeSpecial::create(['spc_name'=>$request->speciality_in_word]);
+            CollegeCourse::create(['clg_id'=>$request->clg_id,'cos_id'=>$newCourse->cos_id,'spc_id'=>$newSpecial->spc_id,]);
+            $request->merge(['cos_id' => $newCourse->cos_id,'spc_id'=>$newSpecial->spc_id,]);
+        }
+        elseif($request->cos_id == 0)
+        {
+            $newCourse = Course::create(['cos_title'=> $request->new_cos,'cos_type' =>'Degree']);
+            CollegeCourse::create(['clg_id'=>$request->clg_id,'cos_id'=>$newCourse->cos_id,'spc_id'=>$request->spc_id,]);
+            $request->merge(['cos_id' => $newCourse->cos_id]);
+
+        }elseif($request->spc_id == 0)
+        {
+            $newSpecial = DegreeSpecial::create(['spc_name'=>$request->speciality_in_word]);
+            CollegeCourse::create(['clg_id'=>$request->clg_id,'cos_id'=>$request->cos_id,'spc_id'=>$newSpecial->spc_id,]);
+            $request->merge(['spc_id'=>$newSpecial->spc_id,]);
+        }
+        
+
         $students = StudentEducationDegree::create($request->all());
         $studentss = StudentProfessional::create($request->all());
 
