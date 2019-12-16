700ba7a6775bc7d8b06d81bab25ba675972cddd5
push0012 <pushpamal0012@gmail.com>
2019-12-16 16:11:02 +0530

    add transaction, requrn reference number and date

--- app/Http/Controllers/GraduationController.php
+++ app/Http/Controllers/GraduationController.php
@@ -25,46 +25,59 @@ class GraduationController extends Controller
     }
     public function store(Request $request)
     {
-        $student = Student::create($request->all());
-        $request->request->add(['stu_id' => $student->stu_id]);
-        
-        if(($request->cos_id == 0) && ($request->spc_id == 0))
+        DB::beginTransaction();
+  
+        try 
         {
-            $newCourse = Course::create(['cos_title'=> $request->new_cos,'cos_type' =>'Degree']);
-            $newSpecial = DegreeSpecial::create(['spc_name'=>$request->speciality_in_word]);
-            CollegeCourse::create(['clg_id'=>$request->clg_id,'cos_id'=>$newCourse->cos_id,'spc_id'=>$newSpecial->spc_id,]);
-            $request->merge(['cos_id' => $newCourse->cos_id,'spc_id'=>$newSpecial->spc_id,]);
+                $student = Student::create($request->all());
+                $request->request->add(['stu_id' => $student->stu_id]);
+                
+                if(($request->cos_id == 0) && ($request->spc_id == 0))
+                {
+                    $newCourse = Course::create(['cos_title'=> $request->new_cos,'cos_type' =>'Degree']);
+                    $newSpecial = DegreeSpecial::create(['spc_name'=>$request->speciality_in_word]);
+                    CollegeCourse::create(['clg_id'=>$request->clg_id,'cos_id'=>$newCourse->cos_id,'spc_id'=>$newSpecial->spc_id,]);
+                    $request->merge(['cos_id' => $newCourse->cos_id,'spc_id'=>$newSpecial->spc_id,]);
+                }
+                elseif($request->cos_id == 0)
+                {
+                    $newCourse = Course::create(['cos_title'=> $request->new_cos,'cos_type' =>'Degree']);
+                    CollegeCourse::create(['clg_id'=>$request->clg_id,'cos_id'=>$newCourse->cos_id,'spc_id'=>$request->spc_id,]);
+                    $request->merge(['cos_id' => $newCourse->cos_id]);
+
+                }elseif($request->spc_id == 0)
+                {
+                    $newSpecial = DegreeSpecial::create(['spc_name'=>$request->speciality_in_word]);
+                    CollegeCourse::create(['clg_id'=>$request->clg_id,'cos_id'=>$request->cos_id,'spc_id'=>$newSpecial->spc_id,]);
+                    $request->merge(['spc_id'=>$newSpecial->spc_id,]);
+                }
+                
+
+                $students = StudentEducationDegree::create($request->all());
+                $studentss = StudentProfessional::create($request->all());
+
+                $languages = $request->language;
+
+                foreach ($languages as $language){ 
+                    StudentLanguage::create([
+                        'language_name' => $language['language_name'],
+                        'write_skill' => $language['write_skill'],
+                        'read_skill' => $language['read_skill'],
+                        'speech_skill' => $language['speech_skill'],
+                        'stu_id' => $student->stu_id
+                        ]);
+                }       
+        $refs = $student->stu_id."".$request->clg_id."".$request->cos_id."".$request->spc_id;
+        $date = $student->created_at;
+        DB::commit();
+        return response()->json(['ref'=>$refs,'date'=>$date], 201);
+        } catch (\Exception $e) {
+
+        // Rollback Transaction
+        DB::rollback();
+        return response()->json($e, 500);
         }
-        elseif($request->cos_id == 0)
-        {
-            $newCourse = Course::create(['cos_title'=> $request->new_cos,'cos_type' =>'Degree']);
-            CollegeCourse::create(['clg_id'=>$request->clg_id,'cos_id'=>$newCourse->cos_id,'spc_id'=>$request->spc_id,]);
-            $request->merge(['cos_id' => $newCourse->cos_id]);
-
-        }elseif($request->spc_id == 0)
-        {
-            $newSpecial = DegreeSpecial::create(['spc_name'=>$request->speciality_in_word]);
-            CollegeCourse::create(['clg_id'=>$request->clg_id,'cos_id'=>$request->cos_id,'spc_id'=>$newSpecial->spc_id,]);
-            $request->merge(['spc_id'=>$newSpecial->spc_id,]);
-        }
-        
-
-        $students = StudentEducationDegree::create($request->all());
-        $studentss = StudentProfessional::create($request->all());
-
-        $languages = $request->language;
-
-        foreach ($languages as $language){ 
-            StudentLanguage::create([
-                'language_name' => $language['language_name'],
-                'write_skill' => $language['write_skill'],
-                'read_skill' => $language['read_skill'],
-                'speech_skill' => $language['speech_skill'],
-                'stu_id' => $student->stu_id
-                ]);
-        }
-
-        return response()->json($studentss, 201);
+        //return response()->json($studentss, 201);
     }
 
     public function pending()
@@ -81,6 +94,10 @@ class GraduationController extends Controller
 
     public function approving(Request $request)
     {
+        DB::beginTransaction();
+  
+        try 
+        {
         $confirm = StudentConfirmDegree::create($request->all());
 
         $go = Student::where('stu_id', $request->stu_id)->update(array('stu_confirm_data' => '1'));
@@ -91,20 +108,39 @@ class GraduationController extends Controller
                                         ->update(array('srg_confirm_data' => '1'));
         $go = StudentProfessional::where('stu_id', $request->stu_id)->update(array('sp_confirm_data' => '1'));
         $goss = StudentLanguage::where('stu_id', $request->stu_id)->update(array('sl_confirm_data' => '1'));
-        return response()->json($goss, 201);
+        DB::commit();
+        return response()->json("Data Saved Successfully", 201);
+        } catch (\Exception $e) {
+
+        // Rollback Transaction
+        DB::rollback();
+        return response()->json($e, 500);
+        }
+        //return response()->json($goss, 201);
     }
 
     public function rejecting(Request $request)
     {
-        
+        DB::beginTransaction();
+  
+        try 
+        {
         $go = Student::where('stu_id', $request->stu_id)->update(array('stu_confirm_data' => '2'));
         $gos = StudentEducationDegree::where('stu_id', $request->stu_id)
                                         ->where('clg_id', $request->clg_id)
                                         ->where('cos_id', $request->cos_id)
                                         ->where('spc_id', $request->spc_id)
-                                        ->update(array('srg_confirm_data' => '1'));
+                                        ->update(array('srg_confirm_data' => '2'));
         $go = StudentProfessional::where('stu_id', $request->stu_id)->update(array('sp_confirm_data' => '2'));
         $goss = StudentLanguage::where('stu_id', $request->stu_id)->update(array('sl_confirm_data' => '2'));
-        return response()->json($goss, 201);
+        DB::commit();
+        return response()->json("data Saved Successfully", 201);
+        } catch (\Exception $e) {
+
+        // Rollback Transaction
+        DB::rollback();
+        return response()->json($e, 500);
+        }
+        //return response()->json($goss, 201);
     }
 }
