51f37f85f528ded82198853d36ef2f8c286384b6
push0012 <pushpamal0012@gmail.com>
2019-12-16 14:10:30 +0530

    first initialize of the page

---
+++ resources/views/insert_form/register_fail.blade.php
@@ -0,0 +1,25 @@
+@extends('layouts.app')
+@section('pageTitle', 'ලියාපදිංචිය සනාත කිරීම')
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-12">
+            
+                <div  style="font-size:1.5em;">{{ __('ලියාපදිංචිය සනාත කිරීම') }}</div>
+                <br>
+                <div class="container-fluid">
+                    <div class="row justify-content-center">
+                        <div class="col-md-12">
+                            <strong>
+                                <p>සමාවන්න. ඔබගේ උපාධිධාරී/ඩිප්ලෝමාධාරී ලියාපදිංචි අයදුම්පත ඉදිරිපත් කිරීම අසාර්ථකයි.</p>
+                                <p>මද වේලාවකට පසුව නැවත උත්සාහ කරන්න.</p>
+                                <p>වැඩි විස්තර සදහා 045-2222173 ඔස්සේ අපව සම්බන්ද කර ගන්න</p>
+                            </strong>
+                        </div>
+                    </div>
+                    <hr>
+                </div>   
+            </div>  
+        </div>
+    </div>
+    @endsection
