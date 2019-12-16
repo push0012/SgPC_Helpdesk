31bf601986dc71c9f3240a7ac2270b0ef79dc483
push0012 <pushpamal0012@gmail.com>
2019-12-16 12:28:17 +0530

    first initialize of the page

---
+++ resources/views/insert_form/register_confirm.blade.php
@@ -0,0 +1,43 @@
+@extends('layouts.app')
+@section('pageTitle', 'ලියාපදිංචිය සනාත කිරීම')
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-12">
+            
+                <div  style="font-size:1.5em;">{{ __('ලියාපදිංචිය සනාත කිරීම') }}</div>
+                <br>
+                <div class="container-fluid">
+                    <div class="row justify-content-center">
+                        <div class="col-md-12">
+                            <strong>
+                            <p>ඔබගේ උපාධිධාරී/ඩිප්ලෝමාධාරී ලියාපදිංචි අයදුම්පත සාර්ථකව අප ගබඩා කරගන්නා ලදී.</p> 
+                            <p>එයට අදාල ඔබගේ ලියාපදිංචි අංකය ලබා ගැනීම සදහා පහත සදහන් යොමු අංකය හා 
+                            ලියාපදිංචි වූ දිනය සදහන් කර ඔබගේ උපාධි/ඩිප්ලෝමා සහතිකයේ (විස්තරාත්මක) පැහැදිලි 
+                            ඡායාරූප/ඡායාරූපයක් sgdcspregister@gmail.com යන Email ලිපිනයට යොමු කරන්න.</P> 
+                            <p>සහතික පත්‍රවල ඡායාරූප ලැබී දින 5ක් තුල ඔබගේ ලියාපදිංචි අංකය ඔබවෙත Email 
+                            මාර්ගයෙන් ලැබීමට සලස්වනු ලැබේ.</p> 
+                            </strong>
+                        </div>
+                    </div>
+                    <hr>
+                    <div class="row justify-content-center">
+                        <div class="col-md-6 row justify-content-center">
+                            <lable>Reference Number&nbsp;:  &nbsp;</lable>
+                            <strong>
+                                4875484521
+                            </strong>
+                        </div>
+                        <div class="col-md-6 row justify-content-center">
+                        <lable>Regster Date&nbsp;: &nbsp; </lable>
+                            <strong>
+                                4875484521
+                            </strong>
+                        </div>
+                    </div>
+                    
+                </div>   
+            </div>  
+        </div>
+    </div>
+    @endsection
