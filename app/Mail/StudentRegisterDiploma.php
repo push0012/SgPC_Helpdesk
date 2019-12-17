99ad8d5d97a4845cc82e979334e7fb7e3db34af8
push0012 <pushpamal0012@gmail.com>
2019-12-17 11:58:18 +0530

    first initialize of the page

---
+++ app/Mail/StudentRegisterDiploma.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Http\Request;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+class StudentRegisterDiploma extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $email;
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct($request)
+    {
+        $this->email = $request;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->subject('Diploma Registration')
+                    ->from('hdcspspc@gmail.com', 'Administrator')
+                    ->to($this->email['email'])
+                    ->view('interact.register_success_diploma');
+    }
+}
