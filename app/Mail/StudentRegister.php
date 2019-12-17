8291bdebc5e0c3ef919e5a566a505ce421335154
push0012 <pushpamal0012@gmail.com>
2019-12-17 11:25:55 +0530

    first initialize of the page

---
+++ app/Mail/StudentRegister.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Http\Request;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+class StudentRegister extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $email;
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct($request)
+    {
+        $this->email = $request;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->subject('Graduate/Diploma Registration')
+                    ->from('hdcspspc@gmail.com', 'Administrator')
+                    ->to($this->email['email'])
+                    ->view('interact.register_success');
+    }
+}
