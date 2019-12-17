1a63fd42fd1b4c3e33829d1f6f73d7632bb3fcf4
push0012 <pushpamal0012@gmail.com>
2019-12-17 16:02:02 +0530

    add methods

--- app/Http/Controllers/AjaxController.php
+++ app/Http/Controllers/AjaxController.php
@@ -4,12 +4,39 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Course;
+use App\DegreeSpecial;
+use DB;
+
 
 class AjaxController extends Controller
 {
-    public function Get_Degree_Courses()
+    public function get_degree_courses($id)
     {
-        $course = Course::where('cos_type','Degree')->get();
-        return response()->json($course, 201);
+        $cozbycollege = DB::table('collegecourseview')
+        ->where('clg_id', $id)
+        ->where('cos_type','Degree')->get();
+        return response()->json($cozbycollege, 201);
+    }
+    public function get_diploma_courses($id)
+    {
+        $cozbycollege = DB::table('collegecourseview')
+        ->where('clg_id', $id)
+        ->where('cos_type','Diploma')->get();
+        return response()->json($cozbycollege, 201);
+    }
+
+    public function all_specialities()
+    {
+        $speciality = DegreeSpecial::all();
+        return response()->json($speciality, 201);
+    }
+
+    public function speciality_by(Request $request)
+    {
+        $cozbycollege = DB::table('collegecoursespecialview')
+            ->where('clg_id', $request['clg_id'])
+            ->where('cos_id', $request['cos_id'])->get();
+
+        return response()->json($cozbycollege, 201);
     }
 }
