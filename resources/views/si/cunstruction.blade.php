14fe2d4ee344d8483bf289fecc419d1a054a25fa
push0012 <pushpamal0012@gmail.com>
2020-09-14 15:41:27 +0530

    under construction

---
+++ resources/views/si/cunstruction.blade.php
@@ -0,0 +1,15 @@
+@extends('si.layouts.app')
+@section('pageTitle', '‍‍රැදී සිටින්න...')
+@section('content')
+<div class="container">
+    <div class="row ">
+        <h3>රැදී සිටින්න...</h3>    
+    </div>
+    <hr>
+    <div class="row justify-content-center align-items-center" >
+        <div class="containers" align="center">
+            <h3>මෙම සේවාව</h3>
+        </div>
+    </div>
+</div>
+@endsection
